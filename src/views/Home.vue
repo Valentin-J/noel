0f5eab6bd2c87e9144d0966f10dbdd029ec3a750
valentin.justin <valentin.justin@elosi.com>
2021-12-24 10:00:37 +0100

    Router history mode, structure

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,10 @@
 <template>
-  <hello-world />
+  <div>test</div>
 </template>
 
 <script lang="ts">
-  import Vue from 'vue'
-  import HelloWorld from '../components/HelloWorld.vue'
+import { Vue, Component } from "vue-property-decorator";
 
-  export default Vue.extend({
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
-  })
+@Component
+export default class Home extends Vue {}
 </script>
