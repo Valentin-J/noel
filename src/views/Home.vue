512b6c86bc13d0a647a66ab740d69b347dd7122d
valentin.justin <valentin.justin@elosi.com>
2021-12-24 13:38:38 +0100

    Accueil

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,32 @@
 <template>
-  <div>test</div>
+  <div class="d-flex justify-center align-center" style="height: 100vh">
+    <v-img
+      id="imgNoel"
+      contain
+      :src="require('/public/images/merry_christmas.png')"
+      @click="questions()"
+      class="animate__animated animate__fadeIn animate__zoomIn"
+      max-height="400"
+      max-width="600"
+    ></v-img>
+  </div>
 </template>
 
 <script lang="ts">
+import "animate.css";
+
 import { Vue, Component } from "vue-property-decorator";
 
 @Component
 export default class Home extends Vue {
+  questions() {
+    this.$router.push("Questions");
+  }
 }
 </script>
+
+<style scoped>
+  #imgNoel:hover{
+    cursor: pointer;
+  }
+</style>
